e0f8a1b8f7ddbcbf64326ccd9b97bb6d3078bf4f
ingaSiu <ingasiud@gmail.com>
2025-03-23 18:45:28 +0200

    finish setting up express server

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
+import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
+import weatherRoutes from './routes/weatherRoutes.js';
 
 dotenv.config();
 
+const app = express();
+app.use(cors());
+app.use(express.json());
+
 const port = process.env.PORT || 5000;
 
-const app = express();
+app.use('/api/weather', weatherRoutes);
 
 app.get('/', (req, res) => {
   res.send('Welcome to the Weather tracker API');
