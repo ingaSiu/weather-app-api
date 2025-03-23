a6a49895388e1221c837df1dba5fc93cdd50809c
ingaSiu <ingasiud@gmail.com>
2025-03-23 18:44:44 +0200

    use routes

--- routes/weatherRoutes.js
+++ routes/weatherRoutes.js
@@ -1,9 +1,11 @@
+import { getCities, getForecast } from '../controllers/weatherController.js';
+
 import express from 'express';
 
 const router = express.Router();
 
-router.get('/cities', (req, res) => res.send({ title: 'GET cities' }));
-router.get('/forecast/:city', (req, res) => res.send({ title: 'GET forecast for a city' }));
+router.get('/cities', getCities);
+router.get('/forecast/:city', getForecast);
 router.post('/log', (req, res) => res.send({ title: 'POST log user selection' }));
 router.get('/popular', (req, res) => res.send({ title: 'GET 3 most popular cities' }));
 
