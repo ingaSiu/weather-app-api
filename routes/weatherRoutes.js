27fc8fd3001d7c0fbfc1f02799b786039d1bd50a
ingaSiu <ingasiud@gmail.com>
2025-03-26 11:44:40 +0200

    use logging middleware on route

--- routes/weatherRoutes.js
+++ routes/weatherRoutes.js
@@ -1,10 +1,11 @@
 import { getCities, getForecast } from '../controllers/weatherController.js';
 
 import express from 'express';
+import loggerMiddleware from '../middleware/loggerMiddleware.js';
 
 const router = express.Router();
 
 router.get('/cities', getCities);
-router.get('/forecast/:city', getForecast);
+router.get('/forecast/:city', loggerMiddleware, getForecast);
 
 export default router;
